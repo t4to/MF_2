a46c6224261f6a9f5645f95ab9bb9fa5d6f720f0
t4to <tato.rodrigues@gmail.com>
2012-11-27 23:22:45 -0200

    Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MF2::Application.config.secret_token = '81d2edebc2ca35f16cc8a2154d7bb8734a032beae86e064af1b8cf8c8550371f3c2333924a55e5b471b81def05f907ebd3d917343b78dc6de8c7a721251c5703'
